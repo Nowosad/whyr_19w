bcbfab75be263e365a080b3986e33e544d915cb1
nowosad <dukekubas@gmail.com>
2019-09-21 13:22:21 +0200

    unzips files

---
+++ code/spatial_vis.R
@@ -0,0 +1,31 @@
+library(sf)
+library(tmap)
+library(spData)
+library(spDataLarge)
+
+tm_shape(nz_elev)  +
+  tm_raster(title = "elev", 
+            style = "cont",
+            palette = "-RdYlGn") +
+  tm_shape(nz) +
+  tm_borders(col = "red", 
+             lwd = 3) +
+  tm_scale_bar(breaks = c(0, 100, 200),
+               size = 1) +
+  tm_compass(position = c("LEFT", "center"),
+             type = "rose", 
+             size = 2) +
+  tm_credits(text = "J. Nowosad, 2019") +
+  tm_layout(main.title = "My map",
+            bg.color = "lightblue",
+            inner.margins = c(0, 0, 0, 0))
+
+# 1. Change the map title from "My map" to "New Zealand".
+# 2. Update the map credits with your own name and today's date.
+# 3. Change the color palette to "BuGn". (You can also try other palettes from http://colorbrewer2.org/)
+# 4. Put the north arrow in the top right corner of the map.
+# 5. Improve the legend title by adding the legend units.
+# 6. Increase the number of breaks in the scale bar.
+# 7. Change the borders' color of the New Zealand's regions to black. 
+# Decrease the line width.
+# 8. Change the background color to any color of your choice.
---
+++ code/whyr_19w.R
@@ -0,0 +1,208 @@
+## pkgs = c(
+##   "sf",
+##   "raster",
+##   "dplyr",
+##   "tmap",
+##   "spData",
+##   "usethis"
+## )
+## to_install = !pkgs %in% installed.packages()
+## if(any(to_install)) {
+##   install.packages(pkgs[to_install])
+## }
+## 
+## install.packages("spDataLarge",
+##            repos = "https://nowosad.github.io/drat/",
+##            type = "source")
+
+
+
+library(sf)
+polska = st_read("data/polska.gpkg")
+
+plot(polska)
+
+polska
+
+library(raster)
+dem = raster("data/polska_srtm.tif")
+
+plot(dem)
+
+dem
+
+library(tmap)
+
+tm_shape(polska) + 
+  tm_polygons()
+
+tm_shape(polska) + 
+  tm_polygons() + 
+  tm_scale_bar(position = c("left", 
+                            "bottom")) 
+
+tm_shape(polska) + 
+  tm_polygons() + 
+  tm_scale_bar(position = c("left", 
+                            "bottom")) + 
+  tm_compass()
+
+tm_shape(polska) + 
+  tm_polygons() + 
+  tm_scale_bar(position = c("left", 
+                            "bottom")) + 
+  tm_compass() +
+  tm_layout(title = "Poland")
+
+## tmap_mode("view")
+
+## tm_shape(polska) +
+##   tm_polygons() +
+##   tm_layout(title = "Poland")
+
+## tmap_mode("plot")
+
+tm_shape(dem) + 
+  tm_raster()
+
+tm_shape(dem) + 
+  tm_raster(style = "cont",
+            midpoint = NA)
+
+tm_shape(dem) + 
+  tm_raster(style = "cont", 
+            midpoint = NA,
+            palette = "-RdYlGn")
+## tmaptools::palette_explorer()
+
+tm_shape(dem) + 
+  tm_raster(style = "fixed",
+            breaks = c(-99, 0, 300,
+                       600, 9999), 
+            midpoint = NA,
+            palette = "-RdYlGn",
+            title = "") + 
+  tm_layout(legend.position = c("LEFT", 
+                                "BOTTOM"))
+
+tm_shape(dem) + 
+  tm_raster(style = "fixed",
+            breaks = c(-99, 0, 300,
+                       600, 9999), 
+            labels = c("Depressions", 
+                       "Plains", 
+                       "Hills",
+                       "Mountains"),
+            midpoint = NA,
+            palette = "-RdYlGn",
+            title = "") + 
+  tm_layout(legend.position = c("LEFT", 
+                                "BOTTOM"))
+
+tm_shape(dem) + 
+  tm_raster(style = "fixed",
+            breaks = c(-99, 0, 300,
+                       600, 9999), 
+            labels = c("Depressions", 
+                       "Plains", 
+                       "Hills",
+                       "Mountains"),
+            midpoint = NA,
+            palette = c("#5E8B73",
+                        "#DAE97A", 
+                        "#EADC70", 
+                        "#AF8D5C"),
+            title = "") + 
+  tm_layout(legend.position = c("LEFT",
+                                "BOTTOM"))
+
+map1 = tm_shape(dem) + 
+  tm_raster(style = "fixed",
+            breaks = c(-99, 0, 300, 600, 9999),
+            labels = c("Depressions", "Plains", "Hills", "Mountains"),
+            midpoint = NA, 
+            palette = c("#5E8B73", "#DAE97A", "#EADC70", "#AF8D5C"),
+            title = "") +
+  tm_layout(legend.position = c("LEFT", "BOTTOM"))
+
+## tmap_save(map1, "my_first_map.png")
+
+library(dplyr)
+
+meteo_data = read.csv("data/polska_meteo_2017.csv", encoding = "UTF-8")
+head(meteo_data)
+
+meteo_stations = st_read("data/polska_stacje.gpkg")
+plot(st_geometry(meteo_stations))
+
+meteo_data_sel = meteo_data %>% 
+  filter(Nazwa.stacji %in% unique(meteo_stations$NAZWA_ST))
+
+
+meteo_data_sel = meteo_data %>% 
+  filter(Nazwa.stacji %in% unique(meteo_stations$NAZWA_ST)) %>% 
+  mutate(data = as.Date(paste0(Rok, "-", Miesiac, "-", Dzien)))
+
+
+
+meteo_data_sel = meteo_data %>% 
+  filter(Nazwa.stacji %in% unique(meteo_stations$NAZWA_ST)) %>% 
+  mutate(data = as.Date(paste0(Rok, "-", Miesiac, "-", Dzien))) %>% 
+  dplyr::select(-Rok, -Miesiac, -Dzien)
+
+
+
+meteo_data_sel = meteo_data %>% 
+  filter(Nazwa.stacji %in% unique(meteo_stations$NAZWA_ST)) %>% 
+  mutate(data = as.Date(paste0(Rok, "-", Miesiac, "-", Dzien))) %>% 
+  dplyr::select(-Rok, -Miesiac, -Dzien) %>% 
+  summarize(tavg = mean(tavg), pressure = mean(pressure))
+
+
+
+meteo_data_sel = meteo_data %>% 
+  filter(Nazwa.stacji %in% unique(meteo_stations$NAZWA_ST)) %>% 
+  mutate(data = as.Date(paste0(Rok, "-", Miesiac, "-", Dzien))) %>% 
+  dplyr::select(-Rok, -Miesiac, -Dzien) %>% 
+  group_by(Kod.stacji) %>% 
+  summarize(tavg = mean(tavg), pressure = mean(pressure))
+
+
+
+meteo = meteo_stations %>% 
+  left_join(meteo_data_sel, by = c("KOD_SZS" = "Kod.stacji"))
+
+
+
+meteo = meteo_stations %>% 
+  inner_join(meteo_data_sel, by = c("KOD_SZS" = "Kod.stacji"))
+
+
+
+plot(dem)
+
+plot(st_geometry(meteo), axes = TRUE)
+
+meteo = meteo_stations %>% 
+  inner_join(meteo_data_sel, by = c("KOD_SZS" = "Kod.stacji")) %>% 
+  st_transform(4326)
+
+tm_shape(dem) +
+  tm_raster() +
+  tm_shape(meteo) +
+  tm_symbols(col = "tavg", palette = "RdBu")
+
+meteo$elev = extract(dem, meteo)
+head(meteo)
+
+lc = raster("data/polska_lc.tif")
+plot(lc)
+
+wlkp_pn = st_read("data/wlkp_pn.gpkg", quiet = TRUE)
+plot(st_geometry(wlkp_pn), axes = TRUE)
+
+wlkp_pn_lc = crop(lc, wlkp_pn)
+plot(wlkp_pn_lc)
+
+wlkp_pn_lc2 = mask(wlkp_pn_lc, wlkp_pn)
+plot(wlkp_pn_lc2)
